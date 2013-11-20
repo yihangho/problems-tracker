09ae9e51995457706e3c521d69327838da2ea639
Yihang Ho <hoyihang5@gmail.com>
2013-11-20 23:05:19 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PsRails::Application.config.secret_key_base = 'bf5b00f6aa1ba31f02bd43264beee34215cb2ce35df8872351a6cf6e044e6472ce65048b4f9e53b4e19cad7e2de62bd07f88438c248f3b96c88fedd9f6aa699c'
