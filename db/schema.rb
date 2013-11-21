072c4e8f9aa3d72bb99c88e9a44252963791dfc8
Yihang Ho <hoyihang5@gmail.com>
2013-11-21 12:13:49 +0800

    Configure Postgresql databases.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131120151938) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "problems", force: true do |t|
     t.string   "name"
     t.string   "link"
