66227d5b0381686153e31cae05403d15984f2c48
Yihang Ho <hoyihang5@gmail.com>
2013-11-21 23:38:46 +0800

    Add add_tag and add_tags methods to Problem model.

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,20 @@
 class Problem < ActiveRecord::Base
   validates :name, :link, presence: true
   has_and_belongs_to_many :tags
+
+  def add_tag(t)
+    obj = Tag.find_by name: t
+    if obj.nil?
+      obj = Tag.create name: t
+    end
+    if tags.find_by(id: obj.id).nil?
+      tags << obj
+    end
+  end
+
+  def add_tags(t)
+    t.each do |n|
+      add_tag n
+    end
+  end
 end
--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 describe Problem do
-  let(:problem) { Problem.new(name: "Lorem ipsum", link: "http://www.example.com") }
+  let(:problem) { Problem.create(name: "Lorem ipsum", link: "http://www.example.com") }
+  let(:problem2) { Problem.create(name: "Problem 2", link: "http://www.example.com/2") }
 
   subject { problem }
 
   it { should respond_to :name }
   it { should respond_to :link }
   it { should respond_to :tags }
+  it { should respond_to :add_tag }
+  it { should respond_to :add_tags }
   it { should be_valid }
 
   describe "when name is empty" do
@@ -18,5 +21,42 @@ describe Problem do
   describe "when link is empty" do
   	before { problem.link = "" }
   	it { should_not be_valid }
+  end
+
+  describe "adding a tag" do
+
+    describe "with invalid information" do
+      it "should not create new tag" do
+        expect { problem.add_tag("") }.not_to change(problem.tags, :count)
+      end
+    end
+
+    describe "with valid information" do
+      it "should create new tag" do
+        expect { problem.add_tag("Graph") }.to change(problem.tags, :count).by(1)
+      end
+
+      describe "but repeated tag" do
+        before { problem.add_tag("Graph") }
+        it "should not create new tag" do
+          expect { problem.add_tag("Graph") }.not_to change(problem.tags, :count)
+        end
+      end
+
+      describe "another problem with the same tag" do
+        before { problem.add_tag("Graph") }
+
+        it "should not create a new tag in table" do
+          expect { problem2.add_tag("Graph") }.not_to change(Tag.all, :count).by(1)
+        end
+      end
+    end
+  end
+
+  describe "adding several tags" do
+    let(:tags) { ["Graph", "DFS", "SCC"] }
+    it "should create new tags" do
+      expect { problem.add_tags(tags) }.to change(problem.tags, :count).by(tags.count)
+    end
   end
 end
