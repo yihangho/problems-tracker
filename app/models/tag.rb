1b53ef602153836633a2b47bf1537baba625c386
Yihang Ho <hoyihang5@gmail.com>
2013-11-22 22:59:08 +0800

    Configure pg_search for Tag.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
   validates :name, presence: true
   has_and_belongs_to_many :problems
+  include PgSearch
+  pg_search_scope :search_by_name, against: :name, using: { tsearch: { prefix: true, any_word: true } }
 end
